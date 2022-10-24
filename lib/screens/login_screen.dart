ede51950a5fd545f93f3e14712eb21759a8b449c
Serg8787 <tsybulnikserg@gmail.com>
2022-10-24 20:25:57 +0200

    add login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -9,6 +10,32 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(hintText: "Login"),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: "Password"),
+            ),
+            SizedBox(height: 20),
+            MaterialButton(
+              color: Colors.blue,
+              onPressed: () {},
+              child: Center(
+                child: Text(
+                  "Далі",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
