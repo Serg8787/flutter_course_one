f6ed5d1a9e0eaeefe1462e36c2e03b8fa7ea94d5
Serg8787 <tsybulnikserg@gmail.com>
2022-10-25 20:44:19 +0200

    start autirizasia

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,32 +8,61 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(hintText: "Login"),
-            ),
-            TextField(
-              decoration: InputDecoration(hintText: "Password"),
-            ),
-            SizedBox(height: 20),
-            MaterialButton(
-              color: Colors.blue,
-              onPressed: () {},
-              child: Center(
-                child: Text(
-                  "Далі",
-                  style: TextStyle(color: Colors.white),
-                ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Login"),
+              TextFormField(validator: (value) {
+                if (value!.isEmpty) return "Это поле не может быть пустым";
+                if (value != "admin") return "Все правильно";
+              }
+               ),
+              SizedBox(height: 15),
+              Text("Пароль"),
+              TextFormField(validator: (value) {
+                if (value!.isEmpty) return "Это поле не может быть пустым";
+                if (value != "12345") return "Все правильно";
+              }
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if(_formKey.currentState!.validate()){
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Good")));
+
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Login failed")));
+                      }
+                    },
+                    child: Center(
+                      child: Text(
+                        "Далі",
+                        style: TextStyle(color: Colors.white),
+                      ),
+
+                    ),
+                  ),
+                  SizedBox(width: 5,),
+                  Checkbox(
+                    value: false, onChanged: (bool? value) {  },
+                  )
+
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
